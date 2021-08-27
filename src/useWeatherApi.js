d9d4676c254f8c5caf56bd11a0445823e7a87c60
naluwn <street.baller06@gmail.com>
2021-08-27 11:35:47 +0800

    feat: finish realtime-weather practice

---
+++ src/useWeatherApi.js
@@ -0,0 +1,101 @@
+import { useState, useEffect, useCallback } from "react";
+
+const fetchWeatherForecast = (cityName) => {
+  return fetch(
+    `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-C9221E8F-F550-4613-ABF9-22B9C794E700&locationName=${cityName}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const locationData = data.records.location[0];
+
+      const weatherElements = locationData.weatherElement.reduce(
+        (needElements, item) => {
+          if (["Wx", "PoP", "CI"].includes(item.elementName)) {
+            needElements[item.elementName] = item.time[0].parameter;
+          }
+          return needElements;
+        },
+        {}
+      );
+
+      return {
+        locationName: locationData.locationName,
+        description: weatherElements.Wx.parameterName,
+        weatherCode: weatherElements.Wx.parameterValue,
+        rainPossibility: weatherElements.PoP.parameterName,
+        comfortability: weatherElements.CI.parameterName
+      };
+    });
+};
+
+const fetchCurrentWeather = (cityName) => {
+  return fetch(
+    `https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-C9221E8F-F550-4613-ABF9-22B9C794E700&locationName=${cityName}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const locationData = data.records.location[0];
+      const weatherElements = locationData.weatherElement.reduce(
+        (needElements, item) => {
+          if (["WDSD", "TEMP", "HUMD"].includes(item.elementName)) {
+            needElements[item.elementName] = item.elementValue;
+          }
+          return needElements;
+        },
+        {}
+      );
+
+      return {
+        observationTime: locationData.time.obsTime,
+        locationName: locationData.locationName,
+        temperature: weatherElements.TEMP,
+        windSpeed: weatherElements.WDSD,
+        humid: weatherElements.HUMD
+      };
+    });
+};
+
+const useWeatherApi = (currentLocation) => {
+  const { locationName, cityName } = currentLocation;
+  const [weatherElement, setWeatherElement] = useState({
+    observationTime: new Date(),
+    locationName: "",
+    humid: 0,
+    temperature: 0,
+    windSpeed: 0,
+    description: "",
+    weatherCode: 0,
+    rainPossibility: 0,
+    comfortability: "",
+    isLoading: true
+  });
+
+  const fetchData = useCallback(() => {
+    const fetchingData = async () => {
+      const [currentWeather, weatherForecast] = await Promise.all([
+        fetchCurrentWeather(locationName),
+        fetchWeatherForecast(cityName)
+      ]);
+      setWeatherElement({
+        ...currentWeather,
+        ...weatherForecast,
+        isLoading: false
+      });
+    };
+
+    setWeatherElement((prevState) => ({
+      ...prevState,
+      isLoading: true
+    }));
+
+    fetchingData();
+  }, [locationName, cityName]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return [weatherElement, fetchData];
+};
+
+export default useWeatherApi;
